a5eb6c91d971a8a7ed1898d1e30ff6a90704fab1
Ryan Mentley <ryan@ryanmentley.com>
2026-03-09 14:59:11 -0700

    Add basic setup for RC files with JDK and aliases
    Includes .bashrc and .zshrc

---
+++ dot_aliases.sh
@@ -0,0 +1,4 @@
+# Common to both zsh and bash
+
+alias ts="tig status"
+alias gc="git commit"
---
+++ dot_shell-common.sh
@@ -0,0 +1,13 @@
+# Common to both zsh and bash
+
+export PATH="$HOME/.local/bin:$PATH"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    [ -s "/Applications/Android Studio.app/Contents/jbr/Contents/Home" ] && export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.aliases.sh
