447947cb310714f90b2bc9e2d4748e9094011356
Ryan Mentley <ryan@ryanmentley.com>
2026-03-23 14:01:20 -0700

    Add ANDROID_HOME, other misc shell-common cleanup

--- dot_shell-common.sh
+++ dot_shell-common.sh
@@ -1,5 +1,9 @@
 # Common to both zsh and bash
 
+# ===================
+#  Utility functions 
+# ===================
+
 # Add a directory to $PATH if it exists
 add_to_path_end() {
     [ -d "$1" ] && export PATH="$PATH:$1"
@@ -8,19 +12,41 @@ add_to_path_start() {
     [ -d "$1" ] && export PATH="$1:$PATH"
 }
 
+
+# ===================
+#  OS-specific stuff 
+# ===================
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Mac
+    [ -s "/Applications/Android Studio.app/Contents/jbr/Contents/Home" ] && export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+    export ANDROID_HOME="$HOME/Library/Android/sdk/"
+else
+    # Linux
+    export ANDROID_HOME="$HOME/Library/Android/sdk/"
+fi
+
+
+# ============
+#  PATH setup
+# ============
 add_to_path_end "$HOME/.local/bin"
-add_to_path_end "$HOME/Library/Android/sdk/platform-tools"
-add_to_path_end "$HOME/Library/Android/sdk/cmdline-tools/latest/bin"
+add_to_path_end "$ANDROID_HOME/platform-tools"
+add_to_path_end "$ANDROID_HOME/cmdline-tools/latest/bin"
 
 [ -x "$(command -v code)" ] && export EDITOR="code -w"
 
-# Mac stuff
-if [[ "$(uname)" == "Darwin" ]]; then
-    [ -s "/Applications/Android Studio.app/Contents/jbr/Contents/Home" ] && export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+
+# =====
+#  nvm
+# =====
+if [ -d "$HOME/.nvm" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# ==============
+#  Load aliases
+# ==============
 source ~/.aliases.sh
