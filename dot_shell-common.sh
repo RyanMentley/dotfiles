a00b5f6691681f5d0544a15c4248df36684a9ff9
Ryan Mentley <ryan@ryanmentley.com>
2026-04-08 17:31:53 -0700

    Add Android build tools to the PATH

--- dot_shell-common.sh
+++ dot_shell-common.sh
@@ -30,8 +30,21 @@ fi
 #  PATH setup
 # ============
 add_to_path_end "$HOME/.local/bin"
-add_to_path_end "$ANDROID_HOME/platform-tools"
-add_to_path_end "$ANDROID_HOME/cmdline-tools/latest/bin"
+# Set up Android tooling on the PATH
+if [ -d "$ANDROID_HOME" ]; then
+    add_to_path_end "$ANDROID_HOME/platform-tools"
+    add_to_path_end "$ANDROID_HOME/cmdline-tools/latest/bin"
+    if [ -d "$ANDROID_HOME/build-tools" ]; then
+        # Build tools only exist in version-numbered directories, so find the latest
+        latest_build_tools_dir=$(
+            find "$ANDROID_HOME/build-tools" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; |
+            sort -t. -k1,1n -k2,2n -k3,3n |
+            tail -n 1
+        )
+        [ -n "$latest_build_tools_dir" ] && add_to_path_end "$ANDROID_HOME/build-tools/$latest_build_tools_dir"
+        unset latest_build_tools_dir
+    fi
+fi
 add_to_path_end "$HOME/bin"
 
 [ -x "$(command -v code)" ] && export EDITOR="code -w"
